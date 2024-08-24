cc1f11046fd78529e3fc5a670890cacb38d1be80
Himanshu-Parangat <himanshuparangat005@gmail.com>
2024-08-24 23:12:04 +0530

    web client

--- testing-env/application/main.go
+++ testing-env/application/main.go
@@ -1,10 +1,53 @@
 package main
 
-import(
+// TODO: recive interface return struct
+import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 )
 
 
-func main(){
-	fmt.Printf("reciving Packets\n")
+
+func getData() (*map[string]interface{}, error) {
+	addressURL := "http://localhost:8090/api/v1/data"
+	result, err := http.Get(addressURL)
+	if err != nil {
+		return nil, fmt.Errorf("error making http request: %w", err)
+	}
+	defer result.Body.Close()
+
+	if result.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code: %d", result.StatusCode)
+	}
+
+	var data map[string]interface{}
+	err = json.NewDecoder(result.Body).Decode(&data)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding JSON: %w", err)
+	}
+
+	return &data, nil
+}
+
+func index(responseProssess http.ResponseWriter, responce *http.Request) {
+	fmt.Fprint(responseProssess, "<h1>hi</h1>")
+}
+
+
+
+func data(responseProssess http.ResponseWriter, responce *http.Request) {
+	data, err := getData()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Fprint(responseProssess, data)
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/data", data)
+	http.ListenAndServe(":8080", nil)
 }
