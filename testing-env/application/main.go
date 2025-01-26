9c3c390934ff55318f67617e2726e34ddd7a1f73
Himanshu-Parangat <himanshuparangat005@gmail.com>
2025-01-26 19:11:25 +0530

    make request to api

--- testing-env/application/main.go
+++ testing-env/application/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"embed"
+	"encoding/json"
+	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
 )
@@ -29,10 +32,52 @@ func index(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "index")
 }
 
+func data(w http.ResponseWriter, r *http.Request)  {
+
+	url := "http://localhost:8090/api/v1/data"
+
+	// get url json response
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(w,"error while feching...\n ")
+		fmt.Fprintf(w,"%v",err)
+		return
+	}
+	defer response.Body.Close()
+
+	// check status code 
+	if response.StatusCode != http.StatusOK {
+		fmt.Fprintf(w,"\nNon-OK HTTP status")
+		fmt.Fprintf(w,"%v\n",response.StatusCode)
+		return
+	}
+
+	// read slice byte data 
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Fprintf(w,"Error reading response body: %v", err)
+		return
+	}
+
+
+	var result map[string]interface{}
+
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		fmt.Fprintf(w,"Error while processing json : %v", err)
+		return
+	}
+
+	fmt.Fprintf(w, "%v\n", result)
+	fmt.Fprintf(w, "\nGot respnce")
+}
+
+
 func main() {
 	log.Println("Server is running on http://localhost:8080")
 
 	http.HandleFunc("/", index)
+	http.HandleFunc("/data", data)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
