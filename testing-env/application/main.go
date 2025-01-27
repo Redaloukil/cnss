de51fc27779a850f24d37b7b3363b9451e292424
Himanshu-Parangat <himanshuparangat005@gmail.com>
2025-01-27 21:16:02 +0530

    added gin

--- testing-env/application/main.go
+++ testing-env/application/main.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-
-func main() {
-	fmt.Println("client running..")
+func message_send(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+	})
 }
 
+func main() {
+
+	fmt.Println("\n\nclient running..")
+	router := gin.Default()
+
+	router.GET("/ping", message_send)
+	router.Run()
+}
