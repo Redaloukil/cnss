5ff5350a281ab9105635e52c13084fc11f0979a2
Himanshu-Parangat <himanshuparangat005@gmail.com>
2025-01-29 14:35:38 +0530

    using bufio to read responce

--- testing-env/application/main.go
+++ testing-env/application/main.go
@@ -1,22 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 )
 
-func message_send(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "pong",
-	})
+func get_data(){
+
+	resp, err := http.Get("http://localhost:8090/api/v1/data")
+	if err != nil {
+		log.Println("\n\nserver is down", err)
+		return
+	} 
+	defer resp.Body.Close()
+
+	fmt.Println("Response status:", resp.Status)
+
+	scanner := bufio.NewScanner(resp.Body)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Println("\n\ncan't read Response body", err)
+		return
+    }
+	
 }
 
+
 func main() {
-
 	fmt.Println("\n\nclient running..")
-	router := gin.Default()
-
-	router.GET("/ping", message_send)
-	router.Run()
+	get_data()
 }
