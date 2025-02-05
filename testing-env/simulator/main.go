c443a5a8006ff9524fe307d9db10a9f449e8f7cc
Himanshu-Parangat <himanshuparangat005@gmail.com>
2025-02-05 17:59:40 +0530

    feat(simulator): fech data from the server
    - simple funtion then make's get request to `http://localhost:8090`
    - the data `key` : `value` is then printed from the Response json.

--- testing-env/simulator/main.go
+++ testing-env/simulator/main.go
@@ -1,10 +1,47 @@
 package main
 
-import(
+import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
+var url string = "http://localhost:8090"
+
+func get_data() map[string]interface{} {
+
+	resp, err := http.Get(url + "/api/v1/data")
+	if err != nil {
+		log.Println("\n\nserver is down", err)
+		return nil
+	}
+	defer resp.Body.Close()
+
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("can't read Response Body:", err)
+		return nil
+	}
+
+	var data map[string]interface{}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		log.Println("can't process json data", err)
+		return nil
+	}
+	fmt.Println("Response status:", resp.Status)
+	return data
+}
 
 func main(){
-	fmt.Printf("flodding Packets\n")
+	data := get_data()
+
+	fmt.Println("\n*---------entry-----------")
+	for key, value := range data{
+		fmt.Println("|",key, ":", value)
+	}
+	fmt.Println("*------------------------------")
 }
